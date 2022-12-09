49a3acd3dc3d8b447c62fc9fb4a52dfb66f79621
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-09 14:56:07 +0300

    Update views.py

--- MRE_Soft/views.py
+++ MRE_Soft/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "html/index.html")
+    
+def index(request):
+    return HttpResponse("MBAJE RWAKA ERIC")
+
+def eric( request):
+    return HttpResponse("Salut Eric")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}")
+    
