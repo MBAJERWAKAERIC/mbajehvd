9012b77c7e02c68edf86aa020328c3da858be541
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-09 17:31:10 +0300

    Update urls.py

--- MRE_Soft/urls.py
+++ MRE_Soft/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
+
     path("", views.index, name="index"),
     path("<str:name>", views.greet, name="greet"),
     path("eric", views.eric, name="eric")
