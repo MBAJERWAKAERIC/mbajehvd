cba455c9575a638c282cd5049cd1a7efa3851b8f
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-11 13:02:33 +0300

    2nd d

--- MRE_Soft/urls.py
+++ MRE_Soft/urls.py
@@ -4,6 +4,6 @@ from . import  views
 urlpatterns = [
 
     path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet"),
+   
     path("eric", views.eric, name="eric")
 ]
