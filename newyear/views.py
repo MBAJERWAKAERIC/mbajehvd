d6ca1cbd469c99030720be6f5633673a86666a09
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-11 14:13:44 +0300

    new year = no

--- newyear/views.py
+++ newyear/views.py
@@ -1,5 +1,9 @@
+import datetime
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.month == 1
+    })
