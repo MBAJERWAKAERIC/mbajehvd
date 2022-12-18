53bebdeb9f7fc1fb213595e91b5dd90521a1849c
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-18 20:25:35 +0300

    exps

--- Jscript/server/index.js
+++ Jscript/server/index.js
@@ -1,6 +1,7 @@
-const { request } = require('express');
 const express = require('express');
 
 const app = express();
 
 app.get('/', (request, response) => response.send("Hello world"));
+
+app.listen(8000);
