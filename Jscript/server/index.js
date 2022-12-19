e353cf3bf0e1388354d9c00c0a716b872e23d50c
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-19 18:59:43 +0300

    test node and npm

--- Jscript/server/index.js
+++ Jscript/server/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.send("Hello world"));
+app.get('/', (request, response) => response.send("Node test and npm"));
 
-app.listen(8000);
+app.listen(8080);
