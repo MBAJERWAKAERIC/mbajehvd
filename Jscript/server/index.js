3e4ee0cc74d86b9eaeb952caf28d677d8403cc0a
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-20 12:15:38 +0300

    first calculator

--- Jscript/server/index.js
+++ Jscript/server/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.sendFile("./index.html"));
+app.get('/', (request, response) => response.sendFile(__dirname + "/index.html"));
 
 app.listen(8080);
