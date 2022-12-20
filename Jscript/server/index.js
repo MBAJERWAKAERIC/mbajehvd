1322b9d58898f64a2d7c1716ab46e57898b8c9d5
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-20 12:49:33 +0300

    get response

--- Jscript/server/index.js
+++ Jscript/server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => response.sendFile(__dirname + "/index.html"));
+app.get("/", (request, response) => response.sendFile(__dirname + "/index.html"));
+
+app.post("/", (request, response) => response.send("Greet POST"));
 
 app.listen(8080);
