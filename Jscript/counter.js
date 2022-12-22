6375b05d28db6b70b790f387e3963520644e6f59
mbajerwakaeric <erickmbanjerwaka@gmail.com>
2022-12-22 19:01:09 +0300

    localstorage

--- Jscript/counter.js
+++ Jscript/counter.js
@@ -1,14 +1,19 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 
-    if (counter % 10 === 0) {
-        alert(`count is now ${counter}`);
-    }
+
 
 }
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
+
+
 });
